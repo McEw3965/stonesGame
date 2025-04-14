b2336ac2c81f04207c9479c64fa9ada0ace83553
McEw3965 <EWAN.MCCARNEY22@bathspa.ac.uk>
2025-04-14 10:15:06 +0100

    Creates GameManager and loads scene on button

---
+++ StonesGame/Assets/gameManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.SceneManagement;
+using System.Net.Sockets;
+
+public class gameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public static gameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (Instance != null)
+        {
+            Debug.LogError("More than one GameManager Instance");
+        }
+        Instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void onSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MultiplayerScene")
+        {
+            Debug.Log("Multiplayer Scene Loaded");
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += onSceneLoaded; // Corrected line
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= onSceneLoaded; // Corrected line
+    }
+
+    public void onlineGame()
+    {
+        SceneManager.LoadScene("MultiplayerScene", LoadSceneMode.Single);
+    }
+
+
+}
